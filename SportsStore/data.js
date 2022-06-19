1b11c3d644067e7db3c3666ea3f2111636b3d1ae
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-06-19 17:21:26 +0300

    All structure is prepared and ready for development

---
+++ SportsStore/data.js
@@ -0,0 +1,71 @@
+module.exports = function() {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Kayak",
+        category: "Watersports",
+        description: "A boat for one person",
+        price: 275,
+      },
+      {
+        id: 2,
+        name: "Lifejacket",
+        category: "Watersports",
+        description: "Protective and fashionable",
+        price: 105,
+      },
+      {
+        id: 3,
+        name: "Soccer-ball",
+        category: "Soccer",
+        description: "FIFA-approved weight and size",
+        price: 80,
+      },
+      {
+        id: 4,
+        name: "Corner Flag",
+        category: "Soccer",
+        description: "Give your playing fields a professional touch",
+        price: 110,
+      },
+      {
+        id: 5,
+        name: "Stadium",
+        category: "Soccer",
+        description: "Flat-packed 35,000 seats stadium",
+        price: 12345000,
+      },
+      {
+        id: 6,
+        name: "Thinking cap",
+        category: "Chess",
+        description: "Improve your brain efficiently by 75%",
+        price: 16,
+      },
+      {
+        id: 7,
+        name: "Unsteady Chair",
+        category: "Chess",
+        description: "Secretly given yor opponent disadvantage",
+        price: 255,
+      },
+      {
+        id: 8,
+        name: "Human Chess Board",
+        category: "Chess",
+        description: "A fun game for the family",
+        price: 475,
+      },
+      {
+        id: 9,
+        name: "Bling Bling King",
+        category: "Chess",
+        description: "Gold-platted, diamond-studded King",
+        price: 1200,
+      },
+    ],
+
+    orders: [],
+  };
+};
